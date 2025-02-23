b3e375d1f3e7f81985f6bf83f9143e1c597e7587
adbihag04 <adbihag1@gmail.com>
2025-02-23 13:58:47 +0800

    login button

--- app/src/main/java/com/ucb/eldroid/farmnook/views/auth/LoginActivity.kt
+++ app/src/main/java/com/ucb/eldroid/farmnook/views/auth/LoginActivity.kt
@@ -2,9 +2,11 @@ package com.ucb.eldroid.farmnook.views.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.ucb.eldroid.farmnook.R
+import com.ucb.eldroid.farmnook.views.menu.BottomNavigationBar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,20 @@ class LoginActivity : AppCompatActivity() {
         forgotPasswordTextView.setOnClickListener {
             val intent = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(intent)
+        }
+
+        val signUpTextView = findViewById<TextView>(R.id.tv_sign_up)
+        signUpTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Navigate to BottomNavigationBar when login button is clicked
+        val loginButton = findViewById<Button>(R.id.btn_login)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, BottomNavigationBar::class.java)
+            startActivity(intent)
+            finish() // This prevents the user from going back to the login screen after logging in
         }
     }
 }
