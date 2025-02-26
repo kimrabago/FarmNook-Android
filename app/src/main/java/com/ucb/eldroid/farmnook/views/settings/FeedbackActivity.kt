cd866405f025e2cc15e18e4dc0f7315f704c66e7
adbihag04 <adbihag1@gmail.com>
2025-02-26 14:37:21 +0800

    settings buttons

--- app/src/main/java/com/ucb/eldroid/farmnook/views/settings/FeedbackActivity.kt
+++ app/src/main/java/com/ucb/eldroid/farmnook/views/settings/FeedbackActivity.kt
@@ -1,6 +1,7 @@
 package com.ucb.eldroid.farmnook.views.settings
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +12,10 @@ class FeedbackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedback)
+
+        val backButton = findViewById<ImageButton>(R.id.btn_back)
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
