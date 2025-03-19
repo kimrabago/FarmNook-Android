c6b2a482d64b7787e7ba2675c7e4d02d903287e3
kimrabago <rabagokim26@gmail.com>
2025-03-19 14:07:11 +0800

    changing files/packages name

---
+++ app/src/main/java/com/ucb/eldroid/farmnook/data/model/ChatItem.kt
@@ -0,0 +1,7 @@
+package com.ucb.eldroid.farmnook.data.model
+
+data class ChatItem(
+    val chatId: String,
+    val userName: String,
+    val lastMessage: String
+)
---
+++ app/src/main/java/com/ucb/eldroid/farmnook/ui/farmer/DeliveryConfirmationActivity.kt
@@ -0,0 +1,16 @@
+package com.ucb.eldroid.farmnook.ui.farmer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ucb.eldroid.farmnook.R
+
+class DeliveryConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery_confirmation)
+
+    }
+}
