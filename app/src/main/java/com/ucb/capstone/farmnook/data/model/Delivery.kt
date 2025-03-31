4776c83798e69757cc0d4bc34026d36b55d65bf0
CaeraDenoir11 <monsterbaxter@gmail.com>
2025-03-31 20:34:23 +0800

    mapping till the admin dashboard

--- app/src/main/java/com/ucb/capstone/farmnook/data/model/Delivery.kt
+++ app/src/main/java/com/ucb/capstone/farmnook/data/model/Delivery.kt
@@ -3,18 +3,19 @@ package com.ucb.capstone.farmnook.data.model
 import com.google.firebase.Timestamp
 
 data class Delivery(
-    var id: String? = null,
-    var pickupLocation: String? = null,
-    var provincePickup: String? = null,
-    var destination: String? = null,
-    var provinceDestination: String? = null,
-    var estimatedTime: String? = null,
-    var totalCost: String? = null,
-    var profileImage: String? = null,
-    var truckType: String? = null,  // Added truck type
-    var productType: String? = null, // Added product type
-    var weight: String? = null,      // Added weight
-    var timestamp: Timestamp? = null
-) {
-    constructor() : this(null, null, null, null, null, null, null, null, null, null, null, null)
-}
+    val id: String? = null,
+    val vehicleType: String? = null,
+    val productType: String? = null,
+    val weight: String? = null, // string instead of Int
+    val timestamp: Timestamp? = null,
+    val pickupLocation: String? = null,
+    val destinationLocation: String? = null,
+    val estimateCost: String? = null,
+    val estimatedTime: String? = null,
+    val scheduledTime: String? = null,
+    val farmerId: String? = null, //the userId
+    val businessId: String? = null,
+    val vehicleID: String? = null,
+    val isAccepted: Boolean? = false,
+    // val profileImage: String? = null // if used
+)
